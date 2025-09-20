efa658c8cb06551c37e9ef36b7ca96ac41d5fb5e
XpertDude <braodby@gmail.com>
2025-09-20 20:16:50 +0100

    commit

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,13 @@
 import Card from "@/components/Card"
-
+import Button from "@/components/Button"
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Button title='small' style='rounded-sm' />
+      <Button title='medium' style='rounded-md' />
+      <Button title='Large' style='rounded-full' />
     </div>
   )
 }
