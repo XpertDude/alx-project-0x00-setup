d8e0fb5ba51b396247009ab173958d20dd1c253c
XpertDude <braodby@gmail.com>
2025-09-20 20:13:46 +0100

    commit

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,10 @@
+import { ButtonProps } from "@/interfaces";
+const Button: React.FC<ButtonProps> = ({ title, style }) => {
+  return (
+    <button className=`${style}`>
+    {title}
+    </button>
+  )
+}
+
+export default Button;
